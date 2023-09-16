2e6ad4c5523bb50fb1b1f4e28660aea232660dd2
KnightOChaos <130263512+KnightOChaos@users.noreply.github.com>
2023-09-16 10:53:20 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,16 +6,76 @@ javascript:(function() {
             <title>Google Classroom</title>
             <style>
                 body {
-                    font-family: Arial, sans-serif;
-                    text-align: center;
-                    background-color: #111;
-                    color: #fff;
-                    margin: 0;
-                    padding: 0;
-                }
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #111;
+            color: #fff;
+            margin: 0;
+            padding: 0;
+        }
 
-                /* ... (paste your CSS here) ... */
+        h1 {
+            color: #007BFF; /* Blue text color */
+        }
 
+        p {
+            color: #ddd;
+        }
+
+        #inputContainer {
+            margin-top: 20px;
+        }
+
+        #youtubeLink {
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            width: 300px;
+            background-color: #333;
+            color: #fff;
+        }
+
+        #embedButton {
+            padding: 10px 20px;
+            background-color: #007BFF;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        #embedButton:hover {
+            background-color: #0056b3;
+        }
+
+        #videoContainer {
+            margin-top: 20px;
+        }
+
+        #historyContainer {
+            margin-top: 20px;
+        }
+
+        .historyItem {
+            margin-bottom: 10px;
+            padding: 5px;
+            background-color: #333;
+            border: 1px solid #007BFF;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .historyItem:hover {
+            background-color: #007BFF;
+        }
+
+        #githubLink {
+            margin-top: 30px;
+            color: #007BFF;
+            text-decoration: none;
+        }
             </style>
         </head>
         <body>
